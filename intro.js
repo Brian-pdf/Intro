8fd7e15cf358f714d8761aecbc59527efb1c72e2
Brian <brianzayas21@gmail.com>
2024-02-16 23:10:52 -0500

    commit

--- intro.js
+++ intro.js
@@ -2,6 +2,6 @@ var firstname, lastname, age;
 firstname = "Brian";
 lastname ="Zayas";
 age = 19;
-alert("My name is"+ firstname + lastname);
-alert("I am"+ age + "years old");
+alert("My name is" + firstname + lastname);
+alert("I am" + age + "years old");
 console.log("Doing my best!")
